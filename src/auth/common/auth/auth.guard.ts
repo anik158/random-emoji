28c75a0ff7d560918194b0a183da00e44c98df77
Ahsan <ahsanan@outlook.com>
2026-04-11 12:21:28 +0600

    simple guard implementation

---
+++ src/auth/common/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { log } from 'console';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+
+    const request = context.switchToHttp().getRequest();
+ 
+    if(request.header(`x-key-api`) !== 'SECRET') {
+      log('AuthGuard: Access denied. Invalid API key.');
+      return false;
+    }
+    
+    console.log('AuthGuard: Access granted. Valid API key.');
+    return true
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthGuard } from './auth/common/auth/auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalGuards(new AuthGuard());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
