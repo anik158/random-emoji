414049fa8faf69b3a0ec8f14af758b7d44a6b3b4
Ahsan <ahsanan@outlook.com>
2026-04-13 12:42:10 +0600

    auth test file

--- src/auth/common/auth/auth.guard.spec.ts
+++ src/auth/common/auth/auth.guard.spec.ts
@@ -1,7 +1,69 @@
 import { AuthGuard } from './auth.guard';
+import { ExecutionContext } from '@nestjs/common';
+import { createMock } from  '@golevelup/ts-jest';
 
 describe('AuthGuard', () => {
+  const authGuard = new AuthGuard();
+
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    expect(authGuard).toBeDefined();
   });
+
+  it('should return true for valid API key', () => {
+    const context = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          header: () => 'SECRET',
+          headers: { 'x-key-api': 'SECRET' },
+        }),
+      }),
+    });
+
+    const result = authGuard.canActivate(context);
+    expect(result).toBe(true);
+  });
+
+    it('should return true for valid API key', () => {
+    const context = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          header: () => 'SECRET', 
+         headers: { 'x-key-api': 'SECRET' },
+        }),
+      }),
+    });
+
+    const result = authGuard.canActivate(context);
+    expect(result).toBe(true);
+  });
+
+
+      it('should return false if there is no header passed in', () => {
+    const context = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          header: () => undefined, 
+          headers: { },
+        }),
+      }),
+    });
+
+    const result = authGuard.canActivate(context);
+    expect(result).toBe(false);
+  });
+
+      it('should return false if api key is invalid', () => {
+    const context = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          header: () => 'INVALID_KEY', 
+        }),
+      }),
+    });
+
+    const result = authGuard.canActivate(context);
+    expect(result).toBe(false);
+  });
+
+
 });
